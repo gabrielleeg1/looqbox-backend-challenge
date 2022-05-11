41f46d0025febeb0508aeffe6a37f66d104c253b
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-05-10 22:10:21 -0300

    Startup project

---
+++ src/main/kotlin/com/gabrielleeg1/looqbox/LooqboxApplication.kt
@@ -0,0 +1,11 @@
+package com.gabrielleeg1.looqbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LooqboxApplication
+
+fun main(args: Array<String>) {
+	runApplication<LooqboxApplication>(*args)
+}
---
+++ src/test/kotlin/com/gabrielleeg1/looqbox/LooqboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gabrielleeg1.looqbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LooqboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
