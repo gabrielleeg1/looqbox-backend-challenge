3af1e6973b4780f1add1dc7671bece97104a0960
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-05-12 11:39:31 -0300

    Fix url in wrong annotation

--- src/main/kotlin/com/gabrielleeg1/looqbox/pokemon/resources/PokemonResource.kt
+++ src/main/kotlin/com/gabrielleeg1/looqbox/pokemon/resources/PokemonResource.kt
@@ -3,10 +3,12 @@ package com.gabrielleeg1.looqbox.pokemon.resources
 import com.gabrielleeg1.looqbox.pokemon.domain.PokemonSearchResult
 import com.gabrielleeg1.looqbox.pokemon.domain.PokemonService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/pokemon")
+@RestController
+@RequestMapping("/pokemon")
 class PokemonResource(val pokemonService: PokemonService) {
     @GetMapping
     suspend fun search(@RequestParam("q") query: String): PokemonSearchResult {
